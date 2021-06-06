a8ccc275d0ae14b7600aca41ae6cee47e41c0fc4
Santiago <santiagorosales1@outlook.com>
2021-06-06 02:50:10 -0300

    last commit

--- src/components/NavBar/NavBarElements.jsx
+++ src/components/NavBar/NavBarElements.jsx
@@ -14,7 +14,6 @@ export const Nav = styled.nav`
     position:sticky;
     top:0;
     z-index:10;
-    
 
 
     @media screen and (max-width: 960px){
@@ -28,7 +27,6 @@ justify-content: flex-end;
 height:80px;
 z-index:1;
 width:100%;
-padding:0 24px;
 max-width:1100px;
 margin-left: 15rem;
 `
